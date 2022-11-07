b749132870e18b94bf19c10f4f1b913e4405e0aa
robinren03 <renyanyu323@163.com>
2022-11-07 16:26:21 +0800

    Add reports

--- os5/src/syscall/process.rs
+++ os5/src/syscall/process.rs
@@ -193,8 +193,6 @@ pub fn sys_spawn(_path: *const u8) -> isize {
         let task = current_task().unwrap();
         let new_task=task.spawn(data);
         let new_pid = new_task.pid.0;
-        let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
-        trap_cx.x[10] = 0;
         add_task(new_task);
         return new_pid as isize;
     }
